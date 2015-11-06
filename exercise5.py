6f82040905cd0dd24d0c0b4e0bb4b1ac8e7a0b6b
Elli Bleeker <elli.bleeker@gmail.com>
2015-11-06 16:46:12 +0100

    lxml succes

--- exercise5.py
+++ exercise5.py
@@ -3,5 +3,11 @@ from lxml import etree as ElementTree
 tree = ElementTree.parse("xml/liefde-tsa.xml")
 root = tree.getroot()
 # opvangen van actie:
-x = root.find(".//div[@type='page']")
-print(x)
+firstPage = root.find(".//div[@type='page']")
+# query firstPage met lxml
+elements = firstPage.xpath("//text()")
+for element in elements:
+    print(element)
+
+
+
