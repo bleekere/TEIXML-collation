30344e1c42cb1f566f9485d89aee7bdd877f4de0
Elli Bleeker <elli.bleeker@gmail.com>
2015-11-11 16:51:38 +0100

    test making list running until top parent of country_data.xml

--- test2.py
+++ test2.py
@@ -5,9 +5,12 @@ data = file.read()
 tree = ElementTree.fromstring(data)
 rank = tree.xpath('.//rank')[0]
 input = rank
+# maak een lege lijst
 output = []
+# zolang de input een parent heeft, get parent
 while input != None:
     input = input.getparent()
+    # voeg steeds een element toe aan de lijst 'output' tot en met de root (bij root, parent=None)
     if input != None:
         output.append(input)
 print(output)
