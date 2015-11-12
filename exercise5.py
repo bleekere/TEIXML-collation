71768260f767e176f7f9e8dbd131b01855d453d8
Elli Bleeker <elli.bleeker@gmail.com>
2015-11-12 11:01:39 +0100

    importeer functie finding_parents_methods, roep functie aan generator om add place="overwritten" uit de parents te filteren print uitkomst generator als list (anders foutmelding wegens leeg element)

--- exercise5.py
+++ exercise5.py
@@ -1,3 +1,5 @@
+# import finding_parents
+from finding_parents_method import finding_parents
 # import lxml Element Tree
 from lxml import etree as ElementTree
 tree = ElementTree.parse("xml/liefde-tsa.xml")
@@ -7,12 +9,20 @@ firstPage = root.find(".//div[@type='page']")
 # query firstPage met lxml (. is belangrijk, verwijst naar eerste div als root)
 textelements = firstPage.xpath(".//text()")
 for textelement in textelements:
-    # if value of place == overwritten, ignore text and tag
-    place = textelement.getparent().get('place')
-    if place == "overwritten" and textelement.is_tail == False:
-        pass
-    else:
-        print(textelement)
+    parents = finding_parents(textelement)
+    elementList = [tag for tag in parents if tag.get("place") == "overwritten"]
+    print(elementList)
+    # add = textelement.getparent().get('place')
+    # if place == "overwritten" and textelement.is_tail == False:
+      #  pass
+    # else:
+      #  print(textelement)
+    # if value of type = instantcorrection
+    # type = textelement.getparent().get('type')
+    # if type == 'instant correction':
+      #  print(type)
+    #  if textelement.getparent().getnext().tag == "add":
+    #   print()
 
 
 
