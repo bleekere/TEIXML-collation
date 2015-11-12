1a280dbb9ea14c1d2caa87a2d252a2cbcb0160ff
Elli Bleeker <elli.bleeker@gmail.com>
2015-11-12 16:46:19 +0100

    importeer functie finding_parents_methods, roep functie aan generator om add place="overwritten" uit de lijst met elementen te filteren als element-waarde != None en element geen tail is: negeer het element Anders: print

--- exercise5.py
+++ exercise5.py
@@ -7,21 +7,23 @@ root = tree.getroot()
 # opvangen van actie:
 firstPage = root.find(".//div[@type='page']")
 # query firstPage met lxml (. is belangrijk, verwijst naar eerste div als root)
-textelements = firstPage.xpath(".//text()")
-for textelement in textelements:
-    parents = finding_parents(textelement)
+textElements = firstPage.xpath(".//text()")
+for textElement in textElements:
+    parents = finding_parents(textElement)
+    # if value of @place = overwritten
     first_or_default = next((tag for tag in parents if tag.get("place") == "overwritten"), None)
-    if first_or_default != None and not textelement.is_tail:
-        pass
-    else:
-        print(textelement)
+    if first_or_default is not None and not textElement.is_tail:
+    # spring uit de lus met continue
+        continue
+    # first_or_default nu alle tags met value of @type = instantcorrection
+    # first_or_default = next((tag for tag in parents if tag.get ("type") == "instant correction"), None)
+    # sibling = first_or_default.getnext()
+    # if sibling.get()
+    # if first_or_default is not None and not textelement.is_tail:
 
-    # if value of type = instantcorrection
-    # type = textelement.getparent().get('type')
-    # if type == 'instant correction':
-      #  print(type)
-    #  if textelement.getparent().getnext().tag == "add":
-    #   print()
+    # else:
+    # print(textelement)
+
 
 
 
