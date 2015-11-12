7bf800fe5fe982d8e165e6fad150f3af7236d3e0
Elli Bleeker <elli.bleeker@gmail.com>
2015-11-12 10:19:10 +0100

    importeer functie finding_parents_methods, roep functie aan

--- country_test2.py
+++ country_test2.py
@@ -1,3 +1,4 @@
+from finding_parents_method import finding_parents
 # import lxml library
 from lxml import etree as ElementTree
 # genereer data voor input later
@@ -5,18 +6,6 @@ file = open('country_data.xml')
 data = file.read()
 tree = ElementTree.fromstring(data)
 rank = tree.xpath('.//rank')[0]
-
-
-def finding_parents(input):
-    # maak een lege lijst
-    output = []
-    # get parent of input
-    while input != None:
-        input = input.getparent()
-        # wanneer input geen parent heeft (root bereikt bij parent=None)
-        if input != None:
-            output.append(input)
-    return output
 
 parents = finding_parents(rank)
 print(parents)
