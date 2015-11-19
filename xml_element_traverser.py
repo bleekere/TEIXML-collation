f496653b91b4b1cdda1fa8bcfa1aad5c9b49ec71
Ronald Haentjens Dekker <ronald.dekker@huygens.knaw.nl>
2015-11-19 16:21:24 +0100

    Added an XML Element Traverser class and an example Visitor implementation.

---
+++ xml_element_traverser.py
@@ -0,0 +1,19 @@
+# XML Element Traverser
+# Class for traversing a XML element tree using a visitor as a callback
+# author: Ronald Haentjens Dekker
+
+
+class XMLElementTraverser(object):
+    def __init__(self, xml_element_visitor):
+        self.xml_element_visitor = xml_element_visitor
+
+    def traverse(self, xml_element):
+        for child in xml_element:
+            self.xml_element_visitor.visit_element(child)
+            if child.text:
+                self.xml_element_visitor.visit_text(child.text)
+            self.traverse(child)
+            self.xml_element_visitor.post_visit_element(child)
+            if child.tail:
+                self.xml_element_visitor.visit_text(child.tail)
+
